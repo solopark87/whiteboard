161e6a174c62bc8fe7f7817a3f8ae315be54b883
Solomon Park <Solomon.Park@outlook.com>
2018-10-17 10:48:58 -0700

    Initial Commit to Repo

---
+++ hello/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+
+            Console.WriteLine("\n\tHelloWorld.Main()\n\t...Continue?...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ whiteboard/whiteboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whiteboard
+{
+    class whiteboard
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\twhiteboard.Main()\n\t...continue?...");
+            Console.ReadKey();
+        }
+    }
+}
