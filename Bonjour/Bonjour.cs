5b8f7c16ceec296beec17e92b859029fb5f3b1e8
Solomon Park <Solomon.Park@outlook.com>
2018-10-17 11:00:50 -0700

    Added Foreign Language Hello World App

---
+++ Bonjour/Bonjour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonjour
+{
+    class Bonjour
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour tous le monde.");
+
+            Console.WriteLine("\n\tBonjour.Main()\n\t...continue?...");
+            Console.ReadKey();
+        }
+    }
+}
