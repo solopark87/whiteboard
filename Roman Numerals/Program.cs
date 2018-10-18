cf4ecb780e8f3f0a9d1a31d28bd2ba499d25c6d1
Solomon Park <Solomon.Park@outlook.com>
2018-10-18 10:22:12 -0700

    Enabled roman numeral capability

---
+++ Roman Numerals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Numerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    class RomanNumerals
+    {
+        //Takes an integer and return a roman numeral for that number
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("\n\tRomanNumerals.Main()\n\t...continue?...");
+            Console.ReadKey();
+
+            Console.WriteLine(romanNumeral(2));
+            Console.ReadLine();
+
+        }
+        public static string romanNumeral (int value)
+        {
+                string result = "";
+                while (value > 0)
+                {
+                    result += 'I';
+                    value--;
+                }
+                return result;
+        }
+
+        
+
+
+    }
+}
